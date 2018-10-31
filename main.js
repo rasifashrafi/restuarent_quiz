838b2ae446b3693a39e4e3b44c57f956eb02a7e0
rasifashrafi <41653841+rasifashrafi@users.noreply.github.com>
2018-10-30 21:40:11 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+$(document).ready( function(){
+var arr = [];
+var name = document.querySelector('.person_name');
+var next = document.querySelector('.next')
+
+
+for(var i =1; i<3 ; i++){
+   arr.push(prompt("enter next name: "));
+   
+}
+alert(arr.join( "<br/>"));
+
+function go(){
+    if(arr.length !== 0){
+        name.innerHTML = '';
+        name.innerHTML = arr.shift();
+    }
+    else{
+        name.innerHTML = 'Restuarent is full'
+    }
+}
+
+next.addEventListener('click',function(e){
+    go();
+})
+
+});
